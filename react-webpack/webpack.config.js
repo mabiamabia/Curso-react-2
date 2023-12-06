c2013b53acedeb23fdcce0dd73b4ec0a1dc3b9ee
mabiamabia <duartecostap@gmail.com>
2023-12-06 19:41:03 -0300

    feat: configurações

--- react-webpack/webpack.config.js
+++ react-webpack/webpack.config.js
@@ -3,9 +3,11 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: path.join(__dirname, "src", "index"),
   output: {
     path: path.join(__dirname, "dist"),
     filename: "bundle.js",
+    publicPath: "/static/",
   },
 };
